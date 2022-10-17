71b805fcffdc13614ab09a0fb6c1d2d5548959dc
tyde81 <pedrohfonseca.1981@gmail.com>
2022-10-17 19:20:03 -0300

    fix: unload bootstrap

--- mix.exs
+++ mix.exs
@@ -1,3 +1,8 @@
+for path <- :code.get_path(),
+    Regex.match?(~r/haxir_bootstrap\-\d+\.\d+\.\d.*\/ebin$/, List.to_string(path)) do
+  Code.delete_path(path)
+end
+
 defmodule HaxirBootstrap.MixProject do
   use Mix.Project
 
